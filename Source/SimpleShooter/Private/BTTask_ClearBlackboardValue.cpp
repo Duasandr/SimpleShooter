a0e6e248638be85a4b5837529d77a47e3f05f9e8
Duasandr <sandro.duarte.alt@gmail.com>
2024-09-25 15:21:25 +0100

    Clear selected key value

--- Source/SimpleShooter/Private/BTTask_ClearBlackboardValue.cpp
+++ Source/SimpleShooter/Private/BTTask_ClearBlackboardValue.cpp
@@ -2,8 +2,22 @@
 
 
 #include "BTTask_ClearBlackboardValue.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 UBTTask_ClearBlackboardValue::UBTTask_ClearBlackboardValue()
 {
 	NodeName = TEXT("Clear Blackboard Value");
+}
+
+EBTNodeResult::Type UBTTask_ClearBlackboardValue::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	EBTNodeResult::Type const Result = Super::ExecuteTask( OwnerComp, NodeMemory );
+	
+	if ( Result == EBTNodeResult::Succeeded )
+	{
+		FName const SelectedKey = GetSelectedBlackboardKey();
+		OwnerComp.GetBlackboardComponent()->ClearValue( SelectedKey );	
+	}
+	
+	return Result;
 }
