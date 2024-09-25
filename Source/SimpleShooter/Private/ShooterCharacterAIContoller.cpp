147b92a59f78932b7c7335013638a3e2845a7efa
Duasandr <sandro.duarte.alt@gmail.com>
2024-09-25 13:13:48 +0100

    Set the players last known location when in sight, otherwise clear it

--- Source/SimpleShooter/Private/ShooterCharacterAIContoller.cpp
+++ Source/SimpleShooter/Private/ShooterCharacterAIContoller.cpp
@@ -9,17 +9,17 @@
 void AShooterCharacterAIContoller::Tick(float DeltaTime)
 {
 	Super::Tick( DeltaTime );
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn( this, 0 );
 
-	// if ( PlayerPawn && LineOfSightTo( PlayerPawn ) )
-	// {
-	// 	SetFocus( PlayerPawn );
-	// 	MoveToActor( PlayerPawn, AcceptanceRadius );
-	// }
-	// else
-	// {
-	// 	ClearFocus( EAIFocusPriority::Default );
-	// 	StopMovement();
-	// }
+	if ( PlayerPawn && LineOfSightTo( PlayerPawn ) )
+	{
+		GetBlackboardComponent()->SetValueAsVector( TEXT( "PlayerLocation" ), PlayerPawn->GetActorLocation() );
+		GetBlackboardComponent()->SetValueAsVector( TEXT( "PlayerLastKnownLocation" ), PlayerPawn->GetActorLocation() );
+	}
+	else
+	{
+		GetBlackboardComponent()->ClearValue( TEXT( "PlayerLocation" ) );
+	}
 }
 
 void AShooterCharacterAIContoller::BeginPlay()
