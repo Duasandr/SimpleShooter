b741d9bc50834e30f21b2a78ddb1476d548e7dbc
Duasandr <sandro.duarte.alt@gmail.com>
2024-09-26 15:41:12 +0100

    Define and implement node tick and default constructor

--- Source/SimpleShooter/Private/BTService_UpdatePlayerLocation.cpp
+++ Source/SimpleShooter/Private/BTService_UpdatePlayerLocation.cpp
@@ -2,4 +2,31 @@
 
 
 #include "BTService_UpdatePlayerLocation.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
+UBTService_UpdatePlayerLocation::UBTService_UpdatePlayerLocation()
+{
+	NodeName = TEXT( "Update Player Location" );
+}
+
+void UBTService_UpdatePlayerLocation::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode( OwnerComp, NodeMemory, DeltaSeconds );
+
+	AAIController const* Owner = OwnerComp.GetAIOwner();
+	if ( !Owner )
+	{
+		return;
+	}
+
+	APawn const* Pawn = Owner->GetPawn();
+	if ( !Pawn )
+	{
+		return;
+	}
+
+	FVector const PawnLocation = Pawn->GetActorLocation();
+	FName const SelectedKey = GetSelectedBlackboardKey();
+	OwnerComp.GetBlackboardComponent()->SetValueAsVector( SelectedKey, PawnLocation );
+}
