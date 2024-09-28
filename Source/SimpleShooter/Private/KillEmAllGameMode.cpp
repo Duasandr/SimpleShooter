fba28204f437a17772ea4f6218213d49afed5ff9
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2024-09-28 12:28:15 +0100

    Declare, define and override pawn killed

--- Source/SimpleShooter/Private/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/Private/KillEmAllGameMode.cpp
@@ -3,3 +3,8 @@
 
 #include "KillEmAllGameMode.h"
 
+void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
+{
+	Super::PawnKilled( PawnKilled );
+	UE_LOG( LogTemp, Warning, TEXT("KillEmAll::PawnKilled") );
+}
