e26928265bc0de59469d3a168e8bc056c48d7aba
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2024-10-03 17:45:53 +0100

    Display widget on game over

--- Source/SimpleShooter/Private/ShooterPlayerController.cpp
+++ Source/SimpleShooter/Private/ShooterPlayerController.cpp
@@ -3,9 +3,18 @@
 
 #include "ShooterPlayerController.h"
 
+#include "Blueprint/UserWidget.h"
+
 void AShooterPlayerController::GameHasEnded(class AActor* EndGameFocus, bool bIsWinner)
 {
 	Super::GameHasEnded( EndGameFocus, bIsWinner );
+
+	// display lose widget
+	UUserWidget* LoseScreenWidget = CreateWidget( this, LoseScreenWidgetClass );
+	if ( LoseScreenWidget )
+	{
+		LoseScreenWidget->AddToViewport();
+	}
 
 	// restart the level after a delay
 	GetWorldTimerManager().SetTimer( RestartTimer, this, &APlayerController::RestartLevel, RestartDelay );
