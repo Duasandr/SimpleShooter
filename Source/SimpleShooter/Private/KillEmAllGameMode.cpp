bbd79eea0f9ec0d1ee947269e3d79a73e99d0ee6
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2024-10-06 13:39:41 +0100

    End the game either with player winning or losing

--- Source/SimpleShooter/Private/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/Private/KillEmAllGameMode.cpp
@@ -4,15 +4,35 @@
 #include "KillEmAllGameMode.h"
 
 #include "EngineUtils.h"
+#include "ShooterCharacter.h"
+#include "ShooterCharacterAIController.h"
 
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
 	Super::PawnKilled( PawnKilled );
+
 	APlayerController* PlayerController = Cast< APlayerController >( PawnKilled->GetController() );
 	if ( PlayerController )
 	{
 		// player died
 		EndGame( false );
+	}
+	else
+	{
+		// ai controller died
+
+		// check for the win condition (all AI controlled characters are dead) 
+		for ( AShooterCharacterAIController* Controller : TActorRange< AShooterCharacterAIController >( GetWorld() ) )
+		{
+			AShooterCharacter const* Character = Cast< AShooterCharacter >( Controller->GetPawn() );
+			if ( Character && !Character->IsDead() )
+			{
+				// at least an AI character is alive
+				return;
+			}
+		}
+		// all the AI characters are dead
+		EndGame( true );
 	}
 }
 
